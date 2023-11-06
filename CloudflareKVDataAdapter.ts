edbcbc7be362b2672290ef178f61204f7bc495bb
Kenny Yi <kenny@statsig.com>
2023-11-06 11:42:42 -0800

    make statsig-node a dev and peer dependency

--- CloudflareKVDataAdapter.ts
+++ CloudflareKVDataAdapter.ts
@@ -1,35 +1,34 @@
-import { KVNamespace } from '@cloudflare/workers-types';
-import { AdapterResponse, IDataAdapter } from 'statsig-node';
+import { KVNamespace } from "@cloudflare/workers-types";
+import { AdapterResponse, IDataAdapter } from "statsig-node";
 
-
-export class CloudflareKVDataAdapter implements IDataAdapter {
+export class CloudflareKVDataAdapter {
   private configSpecsKey: string;
   private kvNamespace: KVNamespace;
   private supportConfigSpecPolling: boolean = false;
 
   public constructor(kvNamespace: KVNamespace, key: string) {
-    this.kvNamespace = kvNamespace; 
+    this.kvNamespace = kvNamespace;
     this.configSpecsKey = key;
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
   public async get(key: string): Promise<AdapterResponse> {
     if (key !== "statsig.cache") {
-      return { error: new Error(`Cloudflare KV Adapter Only Supports Config Specs`) };
+      return {
+        error: new Error(`Cloudflare KV Adapter Only Supports Config Specs`),
+      };
     }
 
     const data = await this.kvNamespace.get(this.configSpecsKey);
     if (data === undefined) {
       return { error: new Error(`key (${key}) does not exist`) };
     }
-    return { result: data ?? undefined, };
+    return { result: data ?? undefined };
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
   public async set(
     key: string,
     value: string,
-    time?: number | undefined,
+    time?: number | undefined
   ): Promise<void> {
     // no-op. Statsig's Edge Config integration keeps config specs synced through Statsig's service
   }
@@ -42,10 +41,7 @@ export class CloudflareKVDataAdapter implements IDataAdapter {
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
-  public async shutdown(): Promise<void> {
-    
-  }
+  public async shutdown(): Promise<void> {}
 
   public supportsPollingUpdatesFor(key: string): boolean {
     if (key === "statsig.cache") {
